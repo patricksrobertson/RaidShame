958574ab5b7a7efa46923dfe6c6fb25c7f013cec
Patrick Robertson <patricksrobertson@gmail.com>
2015-08-03 10:40:45 -0400

    Try a slash cmd.

--- main.lua
+++ main.lua
@@ -1,3 +1,9 @@
-if (IsAddOnLoaded("WeakAuras 2")) then
-  SendChatMessage("WeakAuras loaded!")
+SLASH_RAIDSHAME1 = '/raidshame'
+
+function SlashCmdList.RAIDSHAME(msg, editbox)
+  if (IsAddOnLoaded("WeakAuras 2")) then
+    SendChatMessage("WeakAuras loaded!")
+  else
+    SendChatMessage("WeakAruas not loaded!")
+  end
 end
